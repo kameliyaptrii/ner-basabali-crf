f63780ca5462a14b8ca99523fe88cca58ad50a77
Kameliya Putri <107547896+kameliyaptrii@users.noreply.github.com>
2025-01-07 07:58:31 +0800

    Update model.py

--- model.py
+++ model.py
@@ -14,16 +14,13 @@ def word2features(sent, i):
         'word.lower()': word.lower(),
         'word[-3:]': word[-3:],
         'word[-2:]': word[-2:],
-        'word.isupper()': word.isupper(),
         'word.istitle()': word.istitle(),
-        'word.isdigit()': word.isdigit(),
     }
     if i > 0:
         word1 = sent[i - 1]
         features.update({
             '-1:word.lower()': word1.lower(),
             '-1:word.istitle()': word1.istitle(),
-            '-1:word.isupper()': word1.isupper(),
         })
     else:
         features['BOS'] = True  # Awal kalimat
@@ -33,7 +30,6 @@ def word2features(sent, i):
         features.update({
             '+1:word.lower()': word1.lower(),
             '+1:word.istitle()': word1.istitle(),
-            '+1:word.isupper()': word1.isupper(),
         })
     else:
         features['EOS'] = True  # Akhir kalimat
